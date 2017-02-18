d2cd749935674d8485522918c54a10e04f66b133
Pedro <pedrosantanait@gmail.com>
2017-02-18 14:38:00 -0500

    Added data target classess

--- view.php
+++ view.php
@@ -2,55 +2,66 @@
 
 
 <ul class="nav nav-tabs">
-
   <?php  if (isset($fieldOne) && trim($fieldOne) != "") { ?>  
-  <li class="nav-item">
-    <a class="nav-link active" href="#"><?php  echo h($fieldOne); ?></a>
+  <li class="active">
+    <a href="#filed_one_content_<?php echo $bID;?>" data-toggle="tab"><?php  echo h($fieldOne); ?></a>
   </li>
   <?php  } ?>
 
   <?php  if (isset($fieldTwo) && trim($fieldTwo) != "") { ?>
-  <li class="nav-item">
-    <a class="nav-link" href="#"><?php  echo h($fieldTwo); ?></a>
+  <li>
+    <a href="#filed_two_content_<?php echo $bID;?>" data-toggle="tab"><?php  echo h($fieldTwo); ?></a>
   </li>
   <?php  } ?>
 
   <?php  if (isset($fieldThree) && trim($fieldThree) != "") { ?>
-  <li class="nav-item">
-    <a class="nav-link" href="#"><?php  echo h($fieldThree); ?></a>
+  <li>
+    <a  href="#filed_three_content_<?php echo $bID;?>" data-toggle="tab"><?php  echo h($fieldThree); ?></a>
   </li>
   <?php  } ?>
   <?php  if (isset($fieldFour) && trim($fieldFour) != "") { ?>
-  <li class="nav-item">
-    <a class="nav-link" href="#"><?php  echo h($fieldFour); ?></a>
+  <li>
+    <a  href="#filed_four_content_<?php echo $bID;?>" data-toggle="tab"><?php  echo h($fieldFour); ?></a>
   </li>
   <?php  } ?>
   <?php  if (isset($fieldFive) && trim($fieldFive) != "") { ?>
-  <li class="nav-item">
-    <a class="nav-link" href="#"><?php  echo h($fieldFive); ?></a>
+  <li>
+    <a href="#filed_five_content_<?php echo $bID;?>" data-toggle="tab"><?php  echo h($fieldFive); ?>s</a>
   </li>
   <?php  } ?>
 </ul>
 
+<div class="tab-content">
+    <?php  if (isset($fieldOneContent) && trim($fieldOneContent) != "") { ?>
+        <div id="filed_one_content_<?php echo $bID;?>" class="tab-pane fade in active">
+            <?php  echo $fieldOneContent; ?>
+        </div>
+    <?php  } ?>
+
+    <?php  if (isset($fieldTwoContent) && trim($fieldTwoContent) != "") { ?>
+        <div id="filed_two_content_<?php echo $bID;?>" class="tab-pane fade">
+            <?php  echo $fieldTwoContent; ?>
+        </div>
+    <?php  } ?>
+
+    <?php  if (isset($fieldThreeContent) && trim($fieldThreeContent) != "") { ?>
+        <div id="filed_three_content_<?php echo $bID;?>" class="tab-pane fade">
+          <?php  echo $fieldThreeContent; ?>
+        </div>
+    <?php  } ?>
+
+    <?php  if (isset($fieldFourContent) && trim($fieldFourContent) != "") { ?>
+        <div id="filed_four_content_<?php echo $bID;?>" class="tab-pane fade">
+          <?php  echo $fieldFourContent; ?>
+        </div>
+    <?php  } ?>
+
+    <?php  if (isset($fieldFiveContent) && trim($fieldFiveContent) != "") { ?>
+        <div id="filed_five_content_<?php echo $bID;?>" class="tab-pane fade">
+          <?php  echo $fieldFiveContent; ?>
+        </div>
+    <?php  } ?>   
+</div>
 
 
-<?php  if (isset($fieldOne) && trim($fieldOne) != "") { ?>
-    <?php  echo h($fieldOne); ?><?php  } ?>
-<?php  if (isset($fieldOneContent) && trim($fieldOneContent) != "") { ?>
-    <?php  echo $fieldOneContent; ?><?php  } ?>
-<?php  if (isset($fieldTwo) && trim($fieldTwo) != "") { ?>
-    <?php  echo h($fieldTwo); ?><?php  } ?>
-<?php  if (isset($fieldTwoContent) && trim($fieldTwoContent) != "") { ?>
-    <?php  echo $fieldTwoContent; ?><?php  } ?>
-<?php  if (isset($fieldThree) && trim($fieldThree) != "") { ?>
-    <?php  echo h($fieldThree); ?><?php  } ?>
-<?php  if (isset($fieldThreeContent) && trim($fieldThreeContent) != "") { ?>
-    <?php  echo $fieldThreeContent; ?><?php  } ?>
-<?php  if (isset($fieldFour) && trim($fieldFour) != "") { ?>
-    <?php  echo h($fieldFour); ?><?php  } ?>
-<?php  if (isset($fieldFourContent) && trim($fieldFourContent) != "") { ?>
-    <?php  echo $fieldFourContent; ?><?php  } ?>
-<?php  if (isset($fieldFive) && trim($fieldFive) != "") { ?>
-    <?php  echo h($fieldFive); ?><?php  } ?>
-<?php  if (isset($fieldFiveContent) && trim($fieldFiveContent) != "") { ?>
-    <?php  echo $fieldFiveContent; ?><?php  } ?>
+
